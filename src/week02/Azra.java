2801603e930fe7c46427a86a9216e8f16813f933
azrat <Azratrokic@gmail.com>
2024-02-10 08:28:57 +0100

    Updated from Azra

---
+++ src/week02/Azra.java
@@ -0,0 +1,15 @@
+package week02;
+
+public class Azra {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
