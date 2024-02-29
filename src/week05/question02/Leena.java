8c296331ad5980630038dfeedb6ad9113ddd3664
EbuBxl <isikebu@gmail.com>
2024-02-29 17:46:35 +0100

    created answer to W5 Q2

--- src/week05/question02/Leena.java
+++ src/week05/question02/Leena.java
@@ -5,4 +5,25 @@ public class Leena {
     Write a return method that can reverse  String
     Ex: Reverse("ABCD"); ==> DCBA
      */
+
+
+    public static String reverse(String str){
+
+        String result = "";
+
+        for (int i = str.length()-1; i>=0; i--){
+
+            char ch = str.charAt(i);
+
+            result += ch;
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse("ABCD"));
+    }
 }
