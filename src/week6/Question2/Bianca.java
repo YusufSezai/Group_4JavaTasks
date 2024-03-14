9da018460cc493fec78daafa7cf783ec033abba1
BiaCydeo <bianca.stiop@gmail.com>
2024-03-14 19:11:40 +0100

    Maximum number from an int Array

---
+++ src/week6/Question2/Bianca.java
@@ -0,0 +1,31 @@
+package week6.Question2;
+
+public class Bianca {
+
+    /*
+    Find Maximum:
+  Write a method that can find the maximum number from an int Array.
+     */
+
+    public static int maxNumber(int[] array){
+        int max =array[0];
+
+        for (int each:array) {
+            if(each > max){
+                max = each;
+            }
+        }
+
+        return max;
+
+    }
+
+
+    public static void main(String[] args) {
+        int [] arr = {1,6,9,4,7};
+        System.out.println(maxNumber(arr));
+    }
+
+
+
+}
